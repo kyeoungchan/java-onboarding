6b77d4d64477c2b43dbc490a004b74b13f053089
charles <wkc1002@naver.com>
2023-09-15 15:37:54 +0900

    Problem2 구현

--- src/main/java/onboarding/Problem1.java
+++ src/main/java/onboarding/Problem1.java
@@ -3,8 +3,97 @@ package onboarding;
 import java.util.List;
 
 class Problem1 {
+
+    static int exceptionFlag = 0;
+
     public static int solution(List<Integer> pobi, List<Integer> crong) {
-        int answer = Integer.MAX_VALUE;
-        return answer;
+//        int answer = Integer.MAX_VALUE;
+        int[] pobiArray = pobi.stream().mapToInt(n -> n.intValue()).toArray();
+        int[] crongArray = crong.stream().mapToInt(n -> n.intValue()).toArray();
+
+        int pobiScore = calculateScore(pobiArray);
+        int crongScore = calculateScore(crongArray);
+        if (exceptionFlag == 1) {
+            exceptionFlag = 0;
+            return -1;
+        }
+        return pobiScore > crongScore ? 1 : (pobiScore < crongScore ? 2 : 0);
+
+//        return answer;
     }
+
+    private static int calculateScore(int[] pages) {
+        int left = pages[0];
+        int right = pages[1];
+        boolean isValidated = validate(left, right);
+        if (!isValidated) {
+            exceptionFlag = 1;
+            return 0;
+        }
+        int leftScore = calculateMaximumAddOrMultiply(left);
+        int rightScore = calculateMaximumAddOrMultiply(right);
+        return leftScore > rightScore ? leftScore : rightScore;
+    }
+
+    private static boolean validate(int left, int right) {
+        if (left + 1 != right) {
+            return false;
+        }
+        if (left <= 0 || left >= 400) {
+            return false;
+        }
+        if (right <= 0 || right > 400) {
+            return false;
+        }
+        if (left % 2 == 0 || right % 2 != 0) {
+            return false;
+        }
+        return true;
+    }
+
+    private static int calculateMaximumAddOrMultiply(int page) {
+        int[] digits = splitDigits(page);
+        int add = 0;
+        int multi = 1;
+
+        for (int i = 0; i < digits.length; i++) {
+            add += digits[i];
+            multi *= digits[i];
+        }
+        return add > multi ? add : multi;
+    }
+
+    private static int[] splitDigits(int page) {
+
+        int[] digits = null;
+        if (page / 100 >= 1) {
+            digits = new int[3];
+        } else if (page / 10 >= 1) {
+            digits = new int[2];
+        } else {
+            digits = new int[1];
+        }
+
+        int len = digits.length;
+        int rest = page;
+        for (int i = 0; i < len; i++) {
+            int by = (int) Math.pow(10, (len - i - 1));
+            digits[i] = rest / by;
+            rest %= by;
+        }
+
+        return digits;
+    }
+
+    private static int solution(int[] pobi, int[] crong) {
+        int pobiScore = calculateScore(pobi);
+        int crongScore = calculateScore(crong);
+        if (exceptionFlag == 1) {
+            exceptionFlag = 0;
+            return -1;
+        }
+        return pobiScore > crongScore ? 1 : (pobiScore < crongScore ? 2 : 0);
+    }
+
+
 }
--- src/main/java/onboarding/Problem2.java
+++ src/main/java/onboarding/Problem2.java
@@ -1,8 +1,58 @@
 package onboarding;
 
+/**
+ * 애플리케이션에 필요한 협력
+ *  - 1. 문자열의 중복된 부분을 찾아주는 역할
+ *  - 2. 중복된 부분을 제거하는 역할
+ *  - 3. 중복된 부분이 없어질 때까지 계속 1를 호출해주는 역할
+ */
 public class Problem2 {
     public static String solution(String cryptogram) {
-        String answer = "answer";
+        String answer = callRepetition(cryptogram);
         return answer;
     }
+
+    public static String callRepetition(String cryptogram) {
+        StringBuffer buffer = new StringBuffer(cryptogram);
+
+        while (removeRepetition(buffer)) {
+            System.out.println("buffer = " + buffer);
+        }
+
+        return buffer.toString();
+    }
+
+    public static boolean removeRepetition(StringBuffer buffer) {
+
+        int startIdx = -1;
+        int endIdx = -1;
+        boolean anyRemoved = false;
+
+        for (int i = 0; i < buffer.length() - 1; i++) {
+
+            if (buffer.charAt(i) == buffer.charAt(i + 1)) {
+                if (startIdx == -1) {
+                    startIdx = i;
+                }
+                if (i == buffer.length() - 2) {
+                    // 다음이 마지막 원소라면 더이상 탐색이 안되므로 바로 endIdx를 설정한다.
+                    endIdx = i + 1;
+                }
+            } else {
+                if (startIdx != -1) {
+                    // 더이상 중복이 없을 때 만약 startIdx가 열려있다면 endIdx 설정
+                    endIdx = i;
+                }
+            }
+            if (endIdx != -1) {
+                buffer.delete(startIdx, endIdx + 1);
+                startIdx = -1;
+                endIdx = -1;
+                anyRemoved = true;
+            }
+        }
+
+        return anyRemoved;
+    }
+
 }
